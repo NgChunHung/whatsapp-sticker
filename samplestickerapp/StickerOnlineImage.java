a026e13afd67226440d7111245fbeade70327ba9
NgChunHung <94453663+NgChunHung@users.noreply.github.com>
2023-01-20 17:32:51 +0800

    Add files via upload

---
+++ samplestickerapp/StickerOnlineImage.java
@@ -0,0 +1,30 @@
+package com.example.samplestickerapp;
+import android.content.ContentProvider;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.net.Uri;
+import android.os.StrictMode;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class StickerOnlineImage {
+}
